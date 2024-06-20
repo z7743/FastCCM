db7871231cf20300c8e95fd2ecad7d0e2451560e
z7743 <kapikapycapybara@gmail.com>
2024-06-20 20:36:04 +0900

    formatting

--- src/FastCCM/__init__.py
+++ src/FastCCM/__init__.py
@@ -1,6 +1,5 @@
-from .IMD import LinearProjectionNDim, IMD_nD , RandomTpRangeSubsetDataset
 from .CCM import PairwiseCCM
 
 __all__ = [
-    'PairwiseCCM', "LinearProjectionNDim", "IMD_nD", "RandomTpRangeSubsetDataset"
+    'PairwiseCCM'
 ]
